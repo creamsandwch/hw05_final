00d556526e990f4d2b3df84858b4b99af0426184
creamsandwch <neuronrus1996@gmail.com>
2022-12-07 09:03:24 +0500

    installed django-debug-toolbar==3.2.4 in yatube/venv and freezed updated requirements in yatube/requirements.txt

--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -35,6 +35,7 @@ INSTALLED_APPS = [
     'core.apps.CoreConfig',
     'about.apps.AboutConfig',
     'sorl.thumbnail',
+    'debug_toolbar',
 ]
 
 CACHES = {
@@ -51,6 +52,11 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
+]
+
+INTERNAL_IPS = [
+    '127.0.0.1',
 ]
 
 ROOT_URLCONF = 'yatube.urls'
